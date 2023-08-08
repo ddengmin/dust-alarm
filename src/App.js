4da2b223a3f0ca6d1aa7899e3748c1c06d6ee15e
ddengmin <ammin1127@naver.com>
2023-08-08 23:56:05 +0900

    ♻️ 메인페이지 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BottomMenu from "./component/BottomMenu";
+import Header from "./component/Header";
+import MainPage from "./page/Main"; //지역별 메인 페이지
+import BookMark from "./page/BookMark"; //즐겨찾기 페이지
+import MyPosition from "./page/MyPosition"; //내위치 페이지
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="contents">
+        <Routes>
+          <Route exact path="/" element={<MainPage />} />
+          <Route path="/myPosition" element={<MyPosition />} />
+          <Route path="/bookmark" element={<BookMark />} />
+        </Routes>
+      </div>
+      <BottomMenu />
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import rootReducer from "./module/rootReducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let store = createStore(rootReducer); //전역적으로 어디에서나 사용가능.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
